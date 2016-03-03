deea12ca9c43ae4ec77a9f2b3199d08e670398c9
PENGChunJen <r040921039@ntu.edu.tw>
2016-03-03 22:00:26 +0800

    modify computeMI(p00, p11) and (p01, p10)

--- dsmga2.cpp
+++ dsmga2.cpp
@@ -476,21 +476,24 @@ void DSMGA2::buildGraph() {
             graph.write(i,j,linkage);
             
 		    //2016-02-28	
-			/*
+			
 			cout << "\n i = " << i << " j = " << j << endl;
-			cout << "computeMI(p00,p11) = " << computeMI(p00, 0.0, 0.0, p11) << endl;
-            cout << "computeMI(p10,p10) = " << computeMI(0.0, p01, p10, 0.0) << endl;
+			//cout << "computeMI(p00,p11) = " << computeMI(p00, 0.0, 0.0, p11) << endl;
+            //cout << "computeMI(p10,p10) = " << computeMI(0.0, p01, p10, 0.0) << endl;
+			cout << "computeMI(p00,p11) = " << computeMI(p01+p10, p11, p00, 0.0) << endl;
+            cout << "computeMI(p10,p10) = " << computeMI(p00+p11, p10, p01, 0.0) << endl;
             cout << "           linkage = " << linkage << endl; 
-
-			double linkage0 = computeMI(p00, 0.0, 0.0, p11);
-			double linkage1 = computeMI(0.0, p01, p10, 0.0);
-			if(linkage0 < linkage1)// or linkage0 > linkage1?
-				graph.write(i,j,linkage0);
-			else
-				graph.write(i,j,linkage1);
+            //cout << "         linkage00 = " << 
+			//cout << "         linkage01 = " << 
+			//double linkage0 = computeMI(p00, 0.0, 0.0, p11);
+			//double linkage1 = computeMI(0.0, p01, p10, 0.0);
+			//if(linkage0 < linkage1)// or linkage0 > linkage1?
+			//	graph.write(i,j,linkage0);
+			//else
+			//	graph.write(i,j,linkage1);
             //graph.write(i, j, linkage0, 0);
 			//graph.write(i, j, linkage1, 1);
-			*/
+			
         }
     }
 
